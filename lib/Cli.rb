1949969a7a9a68261825b64e8766122d0f851b0f
Allen70 <Allenlea2007@gmail.com>
2020-11-11 13:58:49 -0700

    Created main_menu & main_exit methods

--- lib/Cli.rb
+++ lib/Cli.rb
@@ -30,7 +30,10 @@ class Cli
     def display_by_name display
         cities = display.pluck(:city)
         cities.each {|city| puts "#{city}"}
-        
+    end
+
+    def display_all_destinations
+        display_by_name(Destination.all)
     end
 
     def select_by_budget
@@ -54,15 +57,47 @@ class Cli
         final_selection = Destination.where budget: @budget, scene: @scene_selection
         display_by_name(final_selection)
     end
+    
+    def main_exit
+        choice = prompt.select('Would you like to exit or go back to the main menu?', %w(main_menu exit))
+            if choice == "main_menu"
+                main_menu
+            else choice == "exit"
+                puts "Thank you for using Covid Traveler to search your destination"
+            end
+    end
 
+        
+    def main_menu
+        selection = prompt.select("What would you like to do?", %w( select_by_budget select_by_scene select_by_budget_and_scene browse_all_destinations))
+            if selection == "select_by_budget"
+                ask_budget
+                select_by_budget
+                display_destinations_by_budget
+                main_exit
+            elsif selection == "select_by_scene"
+                ask_scene
+                select_by_scene
+                display_destinations_by_scene
+                main_exit
+            elsif selection == "select_by_budget_and_scene"
+                ask_scene
+                ask_budget
+                pick_destination_by_budget_scene
+                main_exit
+            elsif selection == "browse_all_destinations"
+                display_all_destinations
+                main_exit
+            end
+    end
     #menu
     #Select_destination_by_budget
-    #Improve Budget
+        #Improve Budget
     #Select By scene
-    #Select x-references
+    #Select by scene & budget
     #Browse by destination
-    
     #exit
+
 
 
 end
